86fddeae4af353373d83fd5153e19f76eeb4ae13
faizie17 <92132180+faizie17@users.noreply.github.com>
2022-03-21 18:32:15 +0530

    Update settings.py

--- designe/settings.py
+++ designe/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
-# import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -33,15 +32,12 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'main',
-    # 'jet.dashboard',
-    # 'jet',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'paypal.standard.ipn'
 ]
 
 MIDDLEWARE = [
@@ -142,12 +138,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL='home'
 LOGOUT_REDIRECT_URL='login'
-
-
-
-
-
-
 
 
 
